c48280efa654ca39246062e1f86fcaaa60bb7d81
guigomes91 <guilherme.gomes91@outlook.com>
2023-02-25 15:53:33 -0300

    Call API in assets file with httpClient

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 
@@ -9,15 +10,13 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [];
+  courses: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
   constructor(private coursesService: CoursesService) {
     this.courses = coursesService.list();
   }
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
 }
