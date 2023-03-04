aa747d7f3a726ad5d092f1b3d1734ed6f286192c
guigomes91 <guilherme.gomes91@outlook.com>
2023-03-04 18:12:04 -0300

    Bussines logic in button add of course

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,9 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { NonNullableFormBuilder } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
@@ -8,20 +12,34 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class CourseFormComponent {
 
-  form: FormGroup;
+  form = this.formBuilder.group({
+    name: [''],
+    category: ['']
+  });
 
-  constructor(private formBuilder: FormBuilder) {
-    this.form = this.formBuilder.group({
-      name: [null],
-      category: [null]
-    });
+  constructor(private formBuilder: NonNullableFormBuilder,
+    private service: CoursesService,
+    private _snackBar: MatSnackBar,
+    private location: Location) {
   }
 
   onSubmit() {
-
+    this.service.save(this.form.value)
+      .subscribe(result => this.onSucess(), error => {
+      this.onError();
+    });
   }
 
   onCancel() {
+    this.location.back();
+  }
 
+  private onSucess() {
+    this._snackBar.open('Curso salvo com sucesso!', '', {duration: 3000});
+    this.onCancel();
+  }
+
+  private onError() {
+    this._snackBar.open('Erro ao salvar curso', '', {duration: 3000});
   }
 }
